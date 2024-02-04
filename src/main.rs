9fdf04f3d63e5fb4d63c5f87e39ca57c0ff5915b
fabien <fabien@cournoyer.club>
2024-02-03 20:46:34 -0500

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, path::Path};
 
 fn main() {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
     let file = args.last().unwrap();
-    let dir = file.trim_end_matches(".msi").to_owned() + "_tmp";
+    let temp_dir = Path::new(file).with_extension("tmp");
     let data = std::fs::read(file).unwrap();
-    fs::create_dir(dir.clone()).unwrap_or_default();
+    fs::create_dir(temp_dir.clone()).unwrap_or_default();
     if data[0..8] != [0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1] {
         panic!("Invalid header!")
     }
@@ -25,10 +25,10 @@ fn main() {
     };
     let _num_dirs = get_u32(&data, 40);
     let _num_fats = get_u32(&data, 44);
-    let mut first_dir = get_u32(&data, 48);
-    let mut first_mini = get_u32(&data, 60);
+    let mut dir_sector = get_u32(&data, 48);
+    let mut minifat_sector = get_u32(&data, 60);
     let _num_mini = get_u32(&data, 64);
-    let mut first_difat = get_u32(&data, 68);
+    let mut difat_entry = get_u32(&data, 68);
     let _num_difat = get_u32(&data, 72);
     let mut difat = vec![];
     for i in 0..109 {
@@ -37,38 +37,41 @@ fn main() {
             difat.push(difat_entry)
         }
     }
-    while first_difat != 0xFFFFFFFE {
+    while difat_entry != 0xFFFFFFFE {
         for i in 0..sector_size / 4 - 1 {
-            let difat_entry = get_u32(&data, sector_size * (first_difat as usize + 1) + i * 4);
-            if difat_entry != 0xFFFFFFFF {
-                difat.push(difat_entry)
+            let difat_entry_int = get_u32(&data, sector_size * (difat_entry as usize + 1) + i * 4);
+            if difat_entry_int != 0xFFFFFFFF {
+                difat.push(difat_entry_int)
             }
         }
-        first_difat = get_u32(&data, sector_size * (first_difat as usize + 1));
+        difat_entry = get_u32(&data, sector_size * (difat_entry as usize + 1));
     }
     let mut fat = vec![];
-    for sector in difat {
+    for difat_sector in difat {
         for i in 0..sector_size / 4 {
-            fat.push(get_u32(&data, sector_size * (sector as usize + 1) + i * 4))
+            fat.push(get_u32(
+                &data,
+                sector_size * (difat_sector as usize + 1) + i * 4,
+            ))
         }
     }
-    let mut mini_fat = vec![];
-    while first_mini != 0xFFFFFFFE {
+    let mut minifat = vec![];
+    while minifat_sector != 0xFFFFFFFE {
         for i in 0..sector_size / 4 {
-            mini_fat.push(get_u32(
+            minifat.push(get_u32(
                 &data,
-                sector_size * (first_mini as usize + 1) + i * 4,
+                sector_size * (minifat_sector as usize + 1) + i * 4,
             ));
         }
-        first_mini = fat[first_mini as usize];
+        minifat_sector = fat[minifat_sector as usize];
     }
     let mut mini_stream = vec![];
-    let mut cabs = vec![];
-    while first_dir != 0xFFFFFFFE {
-        for i in 0..sector_size / 128 {
-            let base = sector_size * (first_dir as usize + 1) + i * 128;
-            let mut sector = get_u32(&data, base + 116);
-            if data[base + 66] == 0x05 {
+    let mut cabinets = vec![];
+    while dir_sector != 0xFFFFFFFE {
+        for dir_entry in 0..sector_size / 128 {
+            let base_offset = sector_size * (dir_sector as usize + 1) + dir_entry * 128;
+            let mut sector = get_u32(&data, base_offset + 116);
+            if data[base_offset + 66] == 0x05 {
                 while sector != 0xFFFFFFFE {
                     mini_stream.append(
                         &mut data[sector_size * (sector as usize + 1)
@@ -79,63 +82,63 @@ fn main() {
                 }
                 continue;
             }
-            if data[base + 66] == 0x01 || data[base + 66] == 0x0 {
+            if data[base_offset + 66] == 0x01 || data[base_offset + 66] == 0x0 {
                 continue;
             }
-            let name_len = get_u16(&data, base + 64);
-            let mut name_u16 = vec![];
+            let name_len = get_u16(&data, base_offset + 64);
+            let mut name_wide = vec![];
             for i in 0..name_len / 2 {
-                name_u16.push(get_u16(&data, base + 2 * i as usize));
+                name_wide.push(get_u16(&data, base_offset + 2 * i as usize));
             }
-            let name = if name_u16.iter().all(|&c| c < 0x3800) {
-                String::from_utf16(&name_u16)
+            let entry_name = if name_wide.iter().all(|&c| c < 0x3800) {
+                String::from_utf16(&name_wide)
                     .unwrap()
                     .trim_end_matches('\0')
                     .trim_start_matches('\x05')
                     .to_owned()
             } else {
                 String::from_utf8(
-                    name_u16
+                    name_wide
                         .iter()
-                        .flat_map(|&c| {
+                        .flat_map(|&chr| {
                             const MAP: &[u8] =
                                 b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz._";
-                            if c == 0 {
+                            if chr == 0 {
                                 return vec![];
                             }
-                            if c == 0x4840 {
+                            if chr == 0x4840 {
                                 return vec![];
                             }
-                            let c = c - 0x3800;
-                            let c1 = c & 0x3F;
-                            let c2 = c >> 6;
-                            let mut res = vec![MAP[c1 as usize]];
-                            if c2 == 0x40 {
-                                return res;
+                            let chr = chr - 0x3800;
+                            let chr1 = chr & 0x3F;
+                            let chr2 = chr >> 6;
+                            let mut bytes = vec![MAP[chr1 as usize]];
+                            if chr2 == 0x40 {
+                                return bytes;
                             }
-                            res.push(MAP[c2 as usize]);
-                            res
+                            bytes.push(MAP[chr2 as usize]);
+                            bytes
                         })
                         .collect::<Vec<_>>(),
                 )
                 .unwrap()
             };
-            let mut size = get_u64(&data, base + 120);
+            let mut entry_size = get_u64(&data, base_offset + 120);
             if version == 3 {
-                size &= 0xffffffff;
+                entry_size &= 0xffffffff;
             }
-            let mut file_data = vec![];
-            if size < 4096 {
+            let mut entry_data = vec![];
+            if entry_size < 4096 {
                 while sector != 0xFFFFFFFE {
-                    file_data.append(
+                    entry_data.append(
                         &mut mini_stream[64 * sector as usize..64 * (sector as usize + 1)]
                             .to_owned(),
                     );
-                    sector = mini_fat[sector as usize];
+                    sector = minifat[sector as usize];
                 }
             } else {
                 while sector != 0xFFFFFFFE {
-                    file_data.append(
+                    entry_data.append(
                         &mut data[sector_size * (sector as usize + 1)
                             ..sector_size * (sector as usize + 2)]
                             .to_owned(),
@@ -143,31 +146,32 @@ fn main() {
                     sector = fat[sector as usize];
                 }
             }
-            fs::write(dir.clone() + "/" + &name, &file_data[0..size as usize]).unwrap();
-            if name.contains(".cab") {
-                let parent = std::path::Path::new(&dir)
-                    .parent()
-                    .unwrap()
-                    .to_string_lossy()
-                    .into_owned();
-                fs::copy(dir.clone() + "/" + &name, parent.clone() + "/" + &name).unwrap();
-                cabs.push(parent.clone() + "/" + &name);
+            fs::write(
+                temp_dir.join(&entry_name),
+                &entry_data[0..entry_size as usize],
+            )
+            .unwrap();
+            if entry_name.contains(".cab") {
+                let parent = std::path::Path::new(&temp_dir).parent().unwrap();
+                fs::copy(temp_dir.join(&entry_name), parent.join(&entry_name)).unwrap();
+                cabinets.push(parent.join(&entry_name).to_string_lossy().into_owned());
             }
         }
-        first_dir = fat[first_dir as usize];
+        dir_sector = fat[dir_sector as usize];
     }
     // Now, extract & rename those files!
-    let strings = fs::read(dir.clone() + "/_StringData").unwrap();
-    let string_infos = fs::read(dir.clone() + "/_StringPool").unwrap();
+    let string_data = fs::read(temp_dir.join("_StringData")).unwrap();
+    let string_pool = fs::read(temp_dir.join("_StringPool")).unwrap();
     let mut offset = 0;
-    let mut string_list = vec![String::new()];
-    for i in 1..string_infos.len() / 4 {
-        let len = get_u16(&string_infos, i * 4);
-        let data = String::from_utf8(strings[offset..offset + len as usize].to_owned()).unwrap();
-        string_list.push(data);
+    let mut strings = vec![String::new()];
+    for index in 1..string_pool.len() / 4 {
+        let len = get_u16(&string_pool, index * 4);
+        let data =
+            String::from_utf8(string_data[offset..offset + len as usize].to_owned()).unwrap();
+        strings.push(data);
         offset += len as usize;
     }
-    let columns = fs::read(dir.clone() + "/_Columns").unwrap();
+    let columns = fs::read(temp_dir.join("_Columns")).unwrap();
     let row_count = columns.len() / 8;
     let mut column_list = vec![];
     for i in 0..row_count {
@@ -177,94 +181,88 @@ fn main() {
         let ty = get_u16(&columns, i * 2 + row_count * 6);
         column_list.push((table, number, name, ty));
     }
-    let directories = fs::read(dir.clone() + "/Directory").unwrap();
-    let columns = get_columns("Directory", &string_list, &column_list);
+    let directory_table = fs::read(temp_dir.join("Directory")).unwrap();
+    let columns = get_columns("Directory", &strings, &column_list);
     let row_length = columns.last().unwrap().1 + columns.last().unwrap().2;
-    let row_count = directories.len() / row_length as usize;
-    let mut directory_list = HashMap::new();
+    let row_count = directory_table.len() / row_length as usize;
+    let mut directories = HashMap::new();
     for i in 0..row_count {
-        let directory_offs = get_val(&directories, i, 0, row_count, 2);
-        let parent_offs = get_val(&directories, i, columns[1].1 as usize, row_count, 2);
-        let dir_default_offs = get_val(&directories, i, columns[2].1 as usize, row_count, 2);
-        directory_list.insert(
-            string_list[directory_offs as usize].as_str(),
+        let directory_offs = get_val(&directory_table, i, 0, row_count, 2);
+        let parent_offs = get_val(&directory_table, i, columns[1].1 as usize, row_count, 2);
+        let dir_default_offs = get_val(&directory_table, i, columns[2].1 as usize, row_count, 2);
+        directories.insert(
+            strings[directory_offs as usize].as_str(),
             (
-                string_list[parent_offs as usize].as_str(),
-                string_list[dir_default_offs as usize].as_str(),
+                strings[parent_offs as usize].as_str(),
+                strings[dir_default_offs as usize].as_str(),
             ),
         );
     }
-    let mut directory_full_list = HashMap::new();
-    for (dir, (parent, name)) in directory_list.clone() {
+    let mut directory_map = HashMap::new();
+    for (dir, (parent, name)) in directories.clone() {
         let mut full_path = String::new();
         let mut parent = parent;
         let mut name = name;
         while parent != "TARGET_DIR" && !parent.is_empty() {
             full_path.insert(0, '/');
             full_path.insert_str(0, name.split_once('|').unwrap_or((name, name)).1);
-            (parent, name) = *directory_list.get(parent).unwrap();
+            (parent, name) = *directories.get(parent).unwrap();
         }
-        directory_full_list.insert(dir, full_path);
+        directory_map.insert(dir, full_path);
     }
-    let component = fs::read(dir.clone() + "/Component").unwrap();
-    let columns = get_columns("Component", &string_list, &column_list);
+    let component_table = fs::read(temp_dir.join("Component")).unwrap();
+    let columns = get_columns("Component", &strings, &column_list);
     let row_length = columns.last().unwrap().1 + columns.last().unwrap().2;
-    let row_count = component.len() / row_length as usize;
-    let mut component_list = HashMap::new();
+    let row_count = component_table.len() / row_length as usize;
+    let mut components = HashMap::new();
     for i in 0..row_count {
-        let component_offs = get_val(&component, i, 0, row_count, 2);
-        let directory_offs = get_val(&component, i, columns[2].1 as usize, row_count, 2);
-        let directory = string_list[directory_offs as usize].as_str();
-        component_list.insert(
-            string_list[component_offs as usize].as_str(),
-            directory_full_list.get(directory).unwrap().as_str(),
+        let component_offs = get_val(&component_table, i, 0, row_count, 2);
+        let directory_offs = get_val(&component_table, i, columns[2].1 as usize, row_count, 2);
+        let directory = strings[directory_offs as usize].as_str();
+        components.insert(
+            strings[component_offs as usize].as_str(),
+            directory_map.get(directory).unwrap().as_str(),
         );
     }
-    let files = fs::read(dir.clone() + "/File").unwrap();
-    let columns = get_columns("File", &string_list, &column_list);
+    let file_table = fs::read(temp_dir.join("File")).unwrap();
+    let columns = get_columns("File", &strings, &column_list);
     let row_length = columns.last().unwrap().1 + columns.last().unwrap().2;
-    let row_count = files.len() / row_length as usize;
-    let mut file_names = HashMap::new();
+    let row_count = file_table.len() / row_length as usize;
+    let mut files = HashMap::new();
     for i in 0..row_count {
-        let file_offs = get_val(&files, i, 0, row_count, 2);
-        let filename_offs = get_val(&files, i, columns[2].1 as usize, row_count, 2);
-        let filename = &string_list[filename_offs as usize];
-        let component_offs = get_val(&files, i, columns[1].1 as usize, row_count, 2);
-        let component = &string_list[component_offs as usize];
+        let file_offs = get_val(&file_table, i, 0, row_count, 2);
+        let filename_offs = get_val(&file_table, i, columns[2].1 as usize, row_count, 2);
+        let filename = &strings[filename_offs as usize];
+        let component_offs = get_val(&file_table, i, columns[1].1 as usize, row_count, 2);
+        let component = &strings[component_offs as usize];
         let long_filename = filename
             .split_once('|')
             .map(|v| v.1)
             .unwrap_or(filename)
             .to_owned();
-        file_names.insert(
-            string_list[file_offs as usize].as_str(),
-            (*component_list.get(component.as_str()).unwrap()).to_owned() + long_filename.as_str(),
+        files.insert(
+            strings[file_offs as usize].as_str(),
+            (*components.get(component.as_str()).unwrap()).to_owned() + long_filename.as_str(),
         );
     }
-    if let Ok(media) = fs::read(dir.clone() + "/Media") {
-        println!("Searching for cabinets...");
-        let columns = get_columns("Media", &string_list, &column_list);
+    if let Ok(media_table) = fs::read(temp_dir.join("Media")) {
+        let columns = get_columns("Media", &strings, &column_list);
         let row_length = columns.last().unwrap().1 + columns.last().unwrap().2;
-        let row_count = media.len() / row_length as usize;
+        let row_count = media_table.len() / row_length as usize;
         for i in 0..row_count {
-            let cabinet = get_val(&media, i, columns[3].1 as usize, row_count, 2);
-            let cab_name = string_list[cabinet as usize].clone();
+            let cabinet = get_val(&media_table, i, columns[3].1 as usize, row_count, 2);
+            let cab_name = strings[cabinet as usize].clone();
             if !cab_name.starts_with('#') && !cab_name.is_empty() {
-                println!("Found cabinet: {cab_name}");
-                let path = std::path::Path::new(&dir)
-                    .parent()
-                    .unwrap()
-                    .to_string_lossy()
-                    .into_owned();
-                cabs.push(path + "/" + &cab_name);
+                let path = Path::new(&temp_dir).parent().unwrap();
+                cabinets.push(path.join(&cab_name).to_string_lossy().into_owned());
             }
         }
     }
-    for cab in cabs {
+    for cab in cabinets {
         println!("Extracting cabinet: {cab}");
-        let base = std::path::Path::new(&cab).parent().unwrap();
+        let base = Path::new(&cab).parent().unwrap();
         let mut cabinet = cab::Cabinet::new(fs::File::open(&cab).unwrap()).unwrap();
-        for (path, name) in &file_names {
+        for (path, name) in &files {
             println!("{name}");
             if let Ok(mut reader) = cabinet.read_file(path) {
                 fs::create_dir_all(base.join("Files").join(name).parent().unwrap()).unwrap();
@@ -273,7 +271,7 @@ fn main() {
             }
         }
     }
-    fs::remove_dir_all(dir).unwrap();
+    fs::remove_dir_all(temp_dir).unwrap();
 }
 fn get_u64(data: &[u8], offs: usize) -> u64 {
     u64::from_le_bytes([
